c2c23a66e4e39919f706078966eb000b280fcaa4
Vlad Titov <eligorchik45@gmail.com>
2024-02-23 02:09:07 +0300

    Add comments

--- train.py
+++ train.py
@@ -11,32 +11,48 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
+
+# Get file paths for anchor, positive, and negative images
 anchor_files = get_files_in_directory(ANC_PATH, NUM_FILES)
 positive_files = get_files_in_directory(POS_PATH, NUM_FILES)
 negative_files = get_files_in_directory(NEG_PATH, NUM_FILES)
 
+# Create datasets for positive and negative pairs
 positive_dataset = SiameseDataset(anchor_files, positive_files, "POS")
 negative_dataset = SiameseDataset(anchor_files, negative_files, "NEG")
 
+# Concatenate positive and negative datasets
 dataset = ConcatDataset([positive_dataset, negative_dataset])
-indices = list(range(len(dataset)))
-train_indices, test_indices = train_test_split(indices, test_size = 0.25, random_state=42)
 
+# Split dataset into train and test sets
+indices = list(range(len(dataset)))
+train_indices, test_indices = train_test_split(indices, test_size=0.25, random_state=42)
+
+# Create train and test subsets
 train_dataset = Subset(dataset, train_indices)
 test_dataset = Subset(dataset, test_indices)
 
-train_loader = DataLoader(train_dataset, batch_size=8, shuffle = True)
-test_loader = DataLoader(test_dataset, batch_size = 8, shuffle = False)
+# Create data loaders for train and test sets
+train_loader = DataLoader(train_dataset, batch_size=8, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=8, shuffle=False)
 
+# Instantiate Siamese Neural Network model
 model = SiameseNN()
-model.to(DEVICE)
+model.to(DEVICE)  # Move model to the specified device (e.g., GPU)
 
-optimizer = optim.Adagrad(model.parameters(), lr=0.001)   
-loss_fn = nn.BCELoss() 
+# Define optimizer and loss function
+optimizer = optim.Adagrad(model.parameters(), lr=0.001)
+loss_fn = nn.BCELoss()  # Binary Cross Entropy Loss
 
+# Specify checkpoint path for saving model checkpoints during training
 checkpoint_path = r"D:\Facial Recognition App\models"
+
+# Train the model with early stopping and get the best training loss
 train_loss = train_model_with_early_stopping(model, train_loader, optimizer, loss_fn, DEVICE, EPOCHS, checkpoint_path)
+
+# Test the trained model and get the mean test loss
 test_loss = test_model(model, test_loader, loss_fn, DEVICE)
 
+# Print the best training loss and mean test loss
 print(f"Best Train Loss: {train_loss:.4f}")
 print(f"Mean Test Loss: {test_loss:.4f}")
