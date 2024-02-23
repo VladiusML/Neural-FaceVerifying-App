db62bb0797e389f59d0291484f4a0fa4d61b7629
Vlad Titov <eligorchik45@gmail.com>
2024-02-23 11:57:49 +0300

    Replace video file by gif and add new folder to make_folder.py

--- scripts/make_folders.py
+++ scripts/make_folders.py
@@ -9,3 +9,5 @@ os.makedirs('data/negative')
 os.makedirs('application_data')
 os.makedirs('application_data/input_image')
 os.makedirs('application_data/verification_images')
+
+os.makedirs('model_weights')
